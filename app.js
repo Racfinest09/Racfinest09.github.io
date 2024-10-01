83cdb55b2e9a6dd3a4c080ba93ab57bcb69ede5e
Racfinest09 <nicolasito1302@hotmail.com>
2024-10-01 12:43:03 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+/*console.log ("Hola mundo");
+var nombre = "Nicolas";
+var apellido = "Ruiz";
+const PI = 3.1415;
+let cantidad = "42"
+
+console.log (nombre);
+console.log (apellido);
+console.log(PI);
+console.log (cantidad);
+console.log ()
+
+let persona ={
+    nombre: "Ana",
+    apellido: "Rodriguez",
+    edad: 30
+};
+
+let lista = ["Carlos","Pedro","Maria","Juan","Nicolas"]
+
+console.log (persona);
+console.log ("Edad de la persona: ",persona.edad);
+console.log ("Nombre de la persona: ",persona.nombre);
+console.log ("Apellido de la persona: ",persona.apellido);
+console.log ("Los compañeros de clase de Ana son: ",lista[0],lista[1],lista[2],lista[3],lista[4]);
+function saludar(){
+    console.log("hola grupo, comienza la clase");
+}
+saludar();
+
+let numero1 = 65
+let numero2 = 70
+console.log (" A continuacion aplicaran las operaciones aritmeticas basicas en los siguientes numeros:")
+let suma = numero1+numero2;
+let resta = numero2-numero1;
+let multiplicacion = numero1*numero2;
+let division = numero1/numero2;
+
+console.log ("Esta es la suma de 65 + 70: ",suma);
+console.log ("Esta es la resta de:",70,"-", 65,"=", resta);
+console.log ("Esta es la multiplicacion de 65 * 70: ",multiplicacion);
+console.log ("Esta es la division de 65 / 70: ",division);
+
+let x = 10;
+console.log (x);
+x +=5; // => x+5=15
+console.log("SUMA",x);
+x *=5; // => x*5 => 50
+console.log ("MULTILICACION",x);
+x -=5; // => x-5 => 45
+console.log ("RESTA",x);
+x /=5; // => x/5 => 2
+console.log ("DIVISION",x);
+
+if(x<5){
+    console.log("Hola");
+}
+else{
+    console.log("Chao");
+}*/
+
+let x = 10;
+let y = 20;
+console.log(x);
+console.log(y);
+
+x++; 
+console.log(x<y);
+console.log(x>y);
+console.log(x!=y);
+console.log(x==y);
+
+
+
+
